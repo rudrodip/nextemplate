3d0b2e349b40d9474bf979961a554a186d08ffd0
rudrodip <official.rudrodipsarker@gmail.com>
2024-01-13 01:09:01 +0600

    Add mobile version not supported message

--- src/components/templates/div-connector/main.tsx
+++ src/components/templates/div-connector/main.tsx
@@ -4,6 +4,9 @@ import { TechStack } from "./stacks";
 export default function Main() {
   return (
     <div className="flex flex-col w-full h-full justify-center items-center">
+      <h1 className="text-center lg:hidden head-text-sm">
+        Mobile version NOT SUPPORTED
+      </h1>
       <TechStack />
     </div>
   );
