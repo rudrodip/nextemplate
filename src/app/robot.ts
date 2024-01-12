732cdc877008b9ab7f4766fe37b2da86ce9d5074
rudrodip <official.rudrodipsarker@gmail.com>
2024-01-12 19:23:05 +0600

    Add robot.ts file for configuring robots.txt

---
+++ src/app/robot.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from "next";
+import { siteConfig } from "@/config/site";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: `${siteConfig.url}/sitemap.xml`,
+  };
+}
