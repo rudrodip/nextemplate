03cc516c0bf707a63ee263ab9b383ca94cea3c75
rudrodip <official.rudrodipsarker@gmail.com>
2024-01-12 23:48:02 +0600

    Update site description in siteConfig

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,7 +3,7 @@ import { SiteConfig } from "@/types";
 export const siteConfig: SiteConfig = {
   name: "NexTemplate",
   description:
-    "Collection of custom templates and components made with Next.JS, shadcn/ui and TailwindCSS.",
+    "Collection of custom templates and components made with Next.JS, shadcn/ui and TailwindCSS",
   url: "https://nextemplate.vercel.app",
   ogImage: "https://nextemplate.vercel.app/og.png",
   creator: {
