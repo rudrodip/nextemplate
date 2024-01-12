b5c83a5b3726c8210d7a983ee0a96a77801cee1d
rudrodip <official.rudrodipsarker@gmail.com>
2024-01-12 19:25:54 +0600

    Add Table of Contents component

---
+++ src/components/mdx/toc.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import * as React from "react";
+
+import { TableOfContents } from "@/lib/toc";
+import { cn } from "@/lib/utils";
+import { useMounted } from "@/hooks/use-mounted";
+import { Separator } from "@/components/ui/separator";
+
+interface TocProps {
+  toc: TableOfContents;
+}
+
+export function DashboardTableOfContents({ toc }: TocProps) {
+  const itemIds = React.useMemo(
+    () =>
+      toc.items
+        ? toc.items
+            .flatMap((item) => [item.url, item?.items?.map((item) => item.url)])
+            .flat()
+            .filter(Boolean)
+            .map((id) => id?.split("#")[1])
+        : [],
+    [toc]
+  );
+  const activeHeading = useActiveItem(itemIds);
+  const mounted = useMounted();
+
+  if (!toc?.items) {
+    return null;
+  }
+
+  return mounted ? (
+    <div className="flex flex-col justify-between py-3 px-5 space-y-2 border-2 border-secondary rounded-lg">
+      <p className="font-bold mb-2">Table of contents</p>
+      <Separator />
+      <Tree tree={toc} activeItem={activeHeading} />
+    </div>
+  ) : null;
+}
+
+function useActiveItem(itemIds: (string | undefined)[]) {
+  const [activeId, setActiveId] = React.useState<string>("");
+
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveId(entry.target.id);
+          }
+        });
+      },
+      { rootMargin: `0% 0% -80% 0%` }
+    );
+
+    itemIds?.forEach((id) => {
+      if (!id) {
+        return;
+      }
+
+      const element = document.getElementById(id);
+      if (element) {
+        observer.observe(element);
+      }
+    });
+
+    return () => {
+      itemIds?.forEach((id) => {
+        if (!id) {
+          return;
+        }
+
+        const element = document.getElementById(id);
+        if (element) {
+          observer.unobserve(element);
+        }
+      });
+    };
+  }, [itemIds]);
+
+  return activeId;
+}
+
+interface TreeProps {
+  tree: TableOfContents;
+  level?: number;
+  activeItem?: string | null;
+}
+
+function Tree({ tree, level = 1, activeItem }: TreeProps) {
+  return tree?.items?.length && level < 3 ? (
+    <ul className={cn("m-0 list-none", { "pl-4": level !== 1 })}>
+      {tree.items.map((item, index) => {
+        return (
+          <li key={index} className={cn("mt-0")}>
+            <a
+              href={item.url}
+              className={cn(
+                "inline-block no-underline px-2 py-2",
+                item.url === `#${activeItem}`
+                  ? "text-primary bg-secondary rounded-sm"
+                  : "text-sm text-muted-foreground"
+              )}
+            >
+              {item.title}
+            </a>
+            {item.items?.length ? (
+              <Tree tree={item} level={level + 1} activeItem={activeItem} />
+            ) : null}
+          </li>
+        );
+      })}
+    </ul>
+  ) : null;
+}
