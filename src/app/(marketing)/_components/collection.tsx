1d6929d37fcf184e1389cf8b68fbbeb24e67633f
rudrodip <official.rudrodipsarker@gmail.com>
2024-01-12 23:32:22 +0600

    Add ComponentDetails import and update fileContentsPromises

--- src/app/(marketing)/_components/collection.tsx
+++ src/app/(marketing)/_components/collection.tsx
@@ -1,6 +1,7 @@
 import { promises as fs } from "fs";
 import path from "path";
 import SearchCollection from "./search-collection";
+import { ComponentDetails } from "@/types";
 
 export default async function TemplateCollection() {
   const componentsDirectory = path.join(
@@ -11,11 +12,10 @@ export default async function TemplateCollection() {
   const components = await fs.readdir(componentsDirectory);
   const fileContentsPromises = components.map(async (file) => {
     const filePath = path.join(componentsDirectory, file);
-    const content = await fs.readFile(
-      path.join(filePath, "component-details.json"),
-      "utf8"
+    const componentDetails: ComponentDetails = JSON.parse(
+      await fs.readFile(path.join(filePath, "component-details.json"), "utf8")
     );
-    return { file: file, content };
+    return { file: file, componentDetails };
   });
 
   const details = await Promise.all(fileContentsPromises);
