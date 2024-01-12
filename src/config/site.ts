cc4fe461f2a0c67556d5985ff6b40a32b98f0986
rudrodip <official.rudrodipsarker@gmail.com>
2024-01-12 19:28:56 +0600

    Add site configuration file

---
+++ src/config/site.ts
@@ -0,0 +1,18 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "NexTemplate",
+  description:
+    "Collection of custom templates and components made with Next.JS, shadcn/ui and TailwindCSS.",
+  url: "https://nextemplate.vercel.app",
+  ogImage: "https://nextemplate.vercel.app/og.png",
+  creator: {
+    name: "RDS",
+    url: "https://twitter.com/rds_agi",
+  },
+  keywords: ["nextjs", "next", "typescript", "tailwindcss"],
+  links: {
+    twitter: "https://twitter.com/rds_agi",
+    github: "https://github.com/rudrodip/nextemplate",
+  },
+};
