ee6fb702e0d88d5e844ae60931c85e2b543725d2
rudrodip <official.rudrodipsarker@gmail.com>
2024-01-12 19:24:41 +0600

    Add TemplateCollection component

---
+++ src/app/(marketing)/_components/collection.tsx
@@ -0,0 +1,28 @@
+import { promises as fs } from "fs";
+import path from "path";
+import SearchCollection from "./search-collection";
+
+export default async function TemplateCollection() {
+  const componentsDirectory = path.join(
+    process.cwd(),
+    "src/components/templates"
+  );
+
+  const components = await fs.readdir(componentsDirectory);
+  const fileContentsPromises = components.map(async (file) => {
+    const filePath = path.join(componentsDirectory, file);
+    const content = await fs.readFile(
+      path.join(filePath, "component-details.json"),
+      "utf8"
+    );
+    return { file: file, content };
+  });
+
+  const details = await Promise.all(fileContentsPromises);
+
+  return (
+    <div className="w-full flex flex-col gap-3 justify-start items-start my-16">
+      <SearchCollection files={details} />
+    </div>
+  );
+}
