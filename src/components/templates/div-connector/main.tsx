d4091c13deba7bc609ec118986652a4479b7a43f
rudrodip <official.rudrodipsarker@gmail.com>
2024-01-12 23:32:44 +0600

    Add div-connector template component

---
+++ src/components/templates/div-connector/main.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { TechStack } from "./stacks";
+
+export default function Main() {
+  return (
+    <div className="flex flex-col w-full h-full justify-center items-center">
+      <TechStack />
+    </div>
+  );
+}
