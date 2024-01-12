00e5faca566d95d61eb8901d9669741c79619577
rudrodip <official.rudrodipsarker@gmail.com>
2024-01-12 19:27:59 +0600

    Add marketing configuration file

---
+++ src/config/marketing.ts
@@ -0,0 +1,18 @@
+import { MarketingConfig } from "@/types";
+
+export const marketingConfig: MarketingConfig = {
+  mainNav: [
+    {
+      title: "About",
+      href: "/about",
+    },
+    {
+      title: "Search",
+      href: "/#search",
+    },
+    {
+      title: "Contact",
+      href: "/#contact",
+    },
+  ],
+};
