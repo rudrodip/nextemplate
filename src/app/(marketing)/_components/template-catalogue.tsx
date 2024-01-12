ec4ac9428aa54a0e8e8eed959b7727245dc17845
rudrodip <official.rudrodipsarker@gmail.com>
2024-01-12 23:32:28 +0600

    Add date to template catalogue component

--- src/app/(marketing)/_components/template-catalogue.tsx
+++ src/app/(marketing)/_components/template-catalogue.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { ComponentDetails } from "@/types";
+import { formatDate } from "@/lib/utils";
 
 type TemplateCatalogueProps = {
   componentDetails: ComponentDetails;
@@ -16,9 +17,12 @@ export const TemplateCatalogue = ({
       href={href}
       className="w-full border rounded p-2 lg:p-4 flex gap-2 justify-start items-center"
     >
-      <div className="flex flex-col justify-start items-start">
+      <div className="flex flex-col justify-start items-start gap-1">
         <h1>{componentDetails.title}</h1>
         <p className="text-sm">{componentDetails.desc}</p>
+        <p className="text-sm text-muted-foreground bg-muted px-1 mt-1 rounded border cursor-pointer">
+          {formatDate(componentDetails.date)}
+        </p>
         <div className="max-w-3xl flex flex-wrap gap-2">
           {componentDetails.tags.map((tag, index) => {
             return (
